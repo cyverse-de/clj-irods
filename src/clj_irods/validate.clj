0d261f06b086287bab5b8b641296cef98178103f
Ian McEwen <mian@cyverse.org>
2020-10-13 23:09:15 -0700

    Add otel span to validate function

--- src/clj_irods/validate.clj
+++ src/clj_irods/validate.clj
@@ -2,6 +2,7 @@
   (:require [clojure-commons.error-codes :as error]
             [clojure.tools.logging :as log]
             [slingshot.slingshot :refer [throw+]]
+            [otel.otel :as otel]
             [clj-jargon.permissions :as jargon-perms]
             [clj-irods.core :as rods]))
 
@@ -23,44 +24,45 @@
   "
   [irods & validations]
 
-  (doseq [v validations]
-    (condp = (first v)
-      :user-exists (let [[users zone] (rest v)]
-                     (doseq [u (if (vector? users) users [users])]
-                       (when (= @(rods/user-type irods u zone) :none)
-                         (throw+ {:error_code error/ERR_NOT_A_USER
-                                  :user u}))))
-      :path-exists (let [[paths user zone] (rest v)]
-                     (doseq [p (if (vector? paths) paths [paths])]
-                       (when (= @(rods/object-type irods user zone p) :none)
-                         (throw+ {:error_code error/ERR_DOES_NOT_EXIST
-                                  :path p}))))
-      :path-is-file (let [[paths user zone] (rest v)]
-                      (doseq [p (if (vector? paths) paths [paths])]
-                        (when-not (= @(rods/object-type irods user zone p) :file)
-                          (throw+ {:error_code error/ERR_NOT_A_FILE
-                                   :path p}))))
-      :path-is-dir (let [[paths user zone] (rest v)]
-                     (doseq [p (if (vector? paths) paths [paths])]
-                       (when-not (= @(rods/object-type irods user zone p) :dir)
-                         (throw+ {:error_code error/ERR_NOT_A_FOLDER
-                                  :path p}))))
-      :path-readable (let [[paths user zone] (rest v)]
+  (otel/with-span [s ["validate"]]
+    (doseq [v validations]
+      (condp = (first v)
+        :user-exists (let [[users zone] (rest v)]
+                       (doseq [u (if (vector? users) users [users])]
+                         (when (= @(rods/user-type irods u zone) :none)
+                           (throw+ {:error_code error/ERR_NOT_A_USER
+                                    :user u}))))
+        :path-exists (let [[paths user zone] (rest v)]
                        (doseq [p (if (vector? paths) paths [paths])]
-                         (when-not (contains? #{:read :write :own} @(rods/permission irods user zone p))
-                           (throw+ {:error_code error/ERR_NOT_READABLE
-                                    :path p
-                                    :user user}))))
-      :path-writeable (let [[paths user zone] (rest v)]
+                         (when (= @(rods/object-type irods user zone p) :none)
+                           (throw+ {:error_code error/ERR_DOES_NOT_EXIST
+                                    :path p}))))
+        :path-is-file (let [[paths user zone] (rest v)]
+                        (doseq [p (if (vector? paths) paths [paths])]
+                          (when-not (= @(rods/object-type irods user zone p) :file)
+                            (throw+ {:error_code error/ERR_NOT_A_FILE
+                                     :path p}))))
+        :path-is-dir (let [[paths user zone] (rest v)]
                        (doseq [p (if (vector? paths) paths [paths])]
-                         (when-not (contains? #{:write :own} @(rods/permission irods user zone p))
-                           (throw+ {:error_code error/ERR_NOT_READABLE
-                                    :path p
-                                    :user user}))))
-      :path-owned (let [[paths user zone] (rest v)]
-                       (doseq [p (if (vector? paths) paths [paths])]
-                         (when-not (= :own @(rods/permission irods user zone p))
-                           (throw+ {:error_code error/ERR_NOT_READABLE
-                                    :path p
-                                    :user user}))))
-      (log/warn "Unrecognized validation type:" (first v)))))
+                         (when-not (= @(rods/object-type irods user zone p) :dir)
+                           (throw+ {:error_code error/ERR_NOT_A_FOLDER
+                                    :path p}))))
+        :path-readable (let [[paths user zone] (rest v)]
+                         (doseq [p (if (vector? paths) paths [paths])]
+                           (when-not (contains? #{:read :write :own} @(rods/permission irods user zone p))
+                             (throw+ {:error_code error/ERR_NOT_READABLE
+                                      :path p
+                                      :user user}))))
+        :path-writeable (let [[paths user zone] (rest v)]
+                         (doseq [p (if (vector? paths) paths [paths])]
+                           (when-not (contains? #{:write :own} @(rods/permission irods user zone p))
+                             (throw+ {:error_code error/ERR_NOT_READABLE
+                                      :path p
+                                      :user user}))))
+        :path-owned (let [[paths user zone] (rest v)]
+                         (doseq [p (if (vector? paths) paths [paths])]
+                           (when-not (= :own @(rods/permission irods user zone p))
+                             (throw+ {:error_code error/ERR_NOT_READABLE
+                                      :path p
+                                      :user user}))))
+        (log/warn "Unrecognized validation type:" (first v))))))
