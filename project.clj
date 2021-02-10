bc06dea232e8afd2947f3aef6f951c1a42b849ad
Sarah Roberts <sarahr@cyverse.org>
2021-02-09 18:21:02 -0700

    CORE-1249: updated some dependencies

--- project.clj
+++ project.clj
@@ -5,15 +5,16 @@
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[test2junit "1.2.2"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :plugins [[jonase/eastwood "0.3.14"]
+            [test2junit "1.2.2"]]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [medley "1.3.0"]
                  [org.cyverse/otel "0.2.0"]
                  [org.cyverse/clojure-commons "2.8.3"]
-                 [org.cyverse/clj-icat-direct "2.9.1"
+                 [org.cyverse/clj-icat-direct "2.9.2-SNAPSHOT"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
-                 [org.cyverse/clj-jargon "2.8.12"
+                 [org.cyverse/clj-jargon "2.8.13-SNAPSHOT"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [slingshot "0.12.2"]])
