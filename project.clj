7de611932b0371c1c2856596b254856890291800
Sarah Roberts <sarahr@cyverse.org>
2021-04-12 21:20:43 -0700

    CORE-1249: updated some dependencies:
    - org.clojure/clojure 1.10.2 -> 1.10.3: I didn't see any breaking changes mentioned in the change log
    - org.cyverse/otel 0.2.0 -> 0.2.3: there aren't any breaking changes in this update
    - org.cyverse/clojure-commons 2.8.3 -> 3.0.6: there aren't any breaking changes in this update that should affect us

--- project.clj
+++ project.clj
@@ -9,10 +9,10 @@
             [test2junit "1.2.2"]]
   :profiles {:repl {:dependencies [[cheshire "5.10.0"]]
                     :source-paths ["repl"]}}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [medley "1.3.0"]
-                 [org.cyverse/otel "0.2.0"]
-                 [org.cyverse/clojure-commons "2.8.3"]
+                 [org.cyverse/otel "0.2.3"]
+                 [org.cyverse/clojure-commons "3.0.6"]
                  [org.cyverse/clj-icat-direct "2.9.2"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
