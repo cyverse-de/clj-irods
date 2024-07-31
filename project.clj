a738b3584e5320341fd5a4c456e2c4614d493bf8
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-30 19:51:31 -0700

    CORE-2006: update the license URL

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure library for interacting with the IRODS data system. Wraps clj-jargon and clj-icat-direct for a nicer interface"
   :url "https://github.com/cyverse-de/clj-irods"
   :license {:name "BSD Standard License"
-            :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :plugins [[jonase/eastwood "1.4.3"]
